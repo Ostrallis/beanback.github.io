327e7e614c5722e44ca9fca30a39a625c629d2b1
EmilSmort <vajaskenyerhun@gmail.com>
2024-07-20 17:18:15 +0200

    added in an old version of beanback sites, new one throws random ass error

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path')
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
 
 app.get('/',(req, res) => {
-    res.send("init");
+    res.sendFile(path.join(__dirname,'index.html'));
 });
 
 app.listen(port, () =>{
